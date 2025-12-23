8a2802993dfc99acf3391476337b1d4e318b26c6
Minsu <zxwhfhddl1092@gmail.com>
2025-12-23 14:52:43 +0900

    슬라이스 이미지 설정

--- src/main/java/com/example/medneduro/z03_Project/Minsu/WebConfig.java
+++ src/main/java/com/example/medneduro/z03_Project/Minsu/WebConfig.java
@@ -8,24 +8,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    // 1. application.properties에 있는 값을 가져옵니다 (@Value 사용)
     @Value("${app.upload-dir}")
     private String uploadDir;
 
     @Value("${app.output-slice-dir}")
     private String sliceDir;
 
+    // 1. [추가] application.properties에서 3D 모델 경로 가져오기
+    @Value("${app.output-obj-dir}")
+    private String objDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-        // 2. 가져온 경로 앞에 "file:///"을 붙여서 웹 리소스로 등록합니다.
 
         // (1) 업로드 폴더 연결 (/mri-file/**)
         registry.addResourceHandler("/mri-file/**")
                 .addResourceLocations("file:///" + uploadDir + "/");
 
-        // (2) 슬라이스 결과 폴더 연결 (/slices/**)
+        // (2) 2D 슬라이스 결과 폴더 연결 (/slices/**)
         registry.addResourceHandler("/slices/**")
                 .addResourceLocations("file:///" + sliceDir + "/");
+
+        // 2. [추가] 3D 모델 결과 폴더 연결 (/obj/**)
+        // -> 이걸 추가해야 404 에러가 사라집니다!
+        registry.addResourceHandler("/obj/**")
+                .addResourceLocations("file:///" + objDir + "/");
     }
 }
